4dbc660abc3443cf7a658e3d03868db9271a508b
MattHamm <mattias@hammarhorn.se>
2025-12-30 22:49:26 +0100

    Added event creation form and styling changes

--- public/main.js
+++ public/main.js
@@ -1,35 +1,127 @@
-// const events = [
-//     {id: '1', title: 'Title 1', category: 'Education', startTime: new Date('2025-11-15T12:00'), endTime: new Date('2025-11-15T19:00'), location: 'Location 1', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...', imgUrl: 'img/pexels-wendywei-1190297.jpg'},
-//     {id: '2', title: 'Title 2', category: 'Festivals', startTime: new Date('2025-11-17T10:00'), endTime: new Date('2025-11-21T16:00'), location: 'Location 2', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...', imgUrl: 'img/pexels-wendywei-1190297.jpg'},
-//     {id: '3', title: 'Title 3', category: 'Food & drinks', startTime: new Date('2025-11-22T19:00'), endTime: new Date('2025-11-22T22:00'), location: 'Location 3', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...', imgUrl: 'img/pexels-wendywei-1190297.jpg'}
-// ];
+const eventForm = document.getElementById('eventForm');
+const eventListDiv = document.getElementById('eventList');
 
 async function loadEvents() {
     try {
-        const response = await fetch('/api/events');
+        const response = await fetch('/api/events', { method: 'GET' });
         if (!response.ok) {
             throw new Error(`Failed to fetch '/api/events'. Status: ${response.response}`);
         }
 
         const events = await response.json();
-        const eventListDiv = document.getElementById('eventList');
-
-        for (let i = 0; i < events.length; i++) {
-            eventListDiv.innerHTML += 
-                `<div class='eventCard'>
-                    <img src='${events[i].imgUrl}'>
-                    <h2>${events[i].title}</h2>
-                    <p>${new Date(events[i].startTime).toTimeString()} - ${new Date(events[i].endTime).toTimeString()}</p>
-                    <p>&#8962; ${events[i].location}</p>
-                    <p>${events[i].description}</p>
-                </div>`;
-        }
-    }
-    catch (error) {
+        populateUserList(events);
+    } catch (error) {
         console.error(`Error fetching '/api/events', ${error}`);
+    }
+}
+
+function populateUserList(events) {
+    eventListDiv.innerHTML = '';
+    for (let i = 0; i < events.length; i++) {
+        let eventImgUrl = events[i].imgUrl ? events[i].imgUrl : 'img/no_img.jpg' ;
+        eventListDiv.innerHTML += 
+            `<div class='eventCard'>
+                <img src='${eventImgUrl}'>
+                <h2>${events[i].title}</h2>
+                <p>${new Date(events[i].startTime).toDateString()} - ${new Date(events[i].endTime).toDateString()}</p>
+                <p>&#8962; ${events[i].location}</p>
+                <p>${events[i].description}</p>
+            </div>`;
+    }
+}
+
+async function postEvent() {
+    if (validateForm()) {
+        var formData = new FormData(eventForm);
+        let eventToAdd = {
+            eventTitle: formData.get('eventTitle'),
+            eventDescription: formData.get('eventDescription'),
+            eventCategory: formData.get('eventCategory'),
+            eventLocation: formData.get('eventLocation'),
+            eventStartTime: formData.get('eventStartTime'),
+            eventEndTime: formData.get('eventEndTime')
+        };
+        console.log(formData);
+
+        try {
+            const response = await fetch('/api/events', {
+                method: 'POST',
+                headers: { 'Content-Type':'application/json' },
+                body: JSON.stringify(eventToAdd)
+            });
+            console.log(response);
+            if (!response.ok) {
+                throw new Error(`Failed to fetch '/api/events'. Status: ${response.response}`);
+            }
+            loadEvents();
+        } catch (error) {
+            console.error(`Error fetching '/api/events', ${error}`);
+        }
     }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     loadEvents();
+
+    eventForm.addEventListener('submit', (e) => {
+        // Since we won't reload the page
+        e.preventDefault();
+        postEvent();
+    });
 });
+
+function validateForm() {
+    var formData = new FormData(eventForm);
+    var isValid = true;
+    let titleError = document.getElementById('titleError');
+    let descriptionError = document.getElementById('descriptionError');
+    let categoryError = document.getElementById('categoryError');
+    let locationError = document.getElementById('locationError');
+
+    if (formData.get('eventTitle') == '') {
+        titleError.innerHTML = 'Title is required.';
+        isValid = false;
+    } else if (formData.get('eventTitle').length < 2 || formData.get('eventTitle').length > 50) {
+        titleError.innerHTML = 'Title must be between 2-50 letters.';
+        isValid = false;
+    } else { titleError.innerHTML = ''; }
+
+    if (formData.get('eventDescription') == '') {
+        descriptionError.innerHTML = 'Description is required.';
+        isValid = false;
+    } else if (formData.get('eventDescription').length > 1000) {
+        descriptionError.innerHTML = 'Description cannot be longer than 1000 letters.';
+        isValid = false;
+    } else { descriptionError.innerHTML = ''; }
+
+    if (formData.get('eventCategory') == '') {
+        categoryError.innerHTML = 'Category is required';
+        isValid = false;
+    } else if (formData.get('eventCategory').length < 2 || formData.get('eventCategory').length > 50) {
+        categoryError.innerHTML = 'Category must be between 2-50 letters.';
+        isValid = false;
+    } else { categoryError.innerHTML = ''; }
+
+    if (formData.get('eventLocation') == '') {
+        let locationError = document.getElementById('locationError');
+        locationError.innerHTML = 'Location is required.';
+        isValid = false;
+    } else if (formData.get('eventLocation').length < 2 || formData.get('eventLocation').length > 50) {
+        locationError.innerHTML = 'Location must be between 2-50 letters.';
+        isValid = false;
+    } else { locationError.innerHTML = ''; }
+
+    if (formData.get('eventStartTime') == '') {
+        let startTimeError = document.getElementById('startTimeError');
+        startTimeError.innerHTML = 'Start Time is required.';
+        isValid = false;
+    } else { startTimeError.innerHTML = ''; }
+
+    if (formData.get('eventEndTime') == '') {
+        let endTimeError = document.getElementById('endTimeError');
+        endTimeError.innerHTML = 'End Time is required.';
+        isValid = false;
+    } else { endTimeError.innerHTML = ''; }
+
+    return isValid;
+}
--- src/eventService.js
+++ src/eventService.js
@@ -9,3 +9,21 @@ export function getEvents() {
     const events = fs.readFileSync(path.join(__dirPath, '../data/events.json'), 'utf-8');
     return JSON.parse(events);
 }
+
+export function createEvent(eventToAdd) {
+    const events = fs.readFileSync(path.join(__dirPath, '../data/events.json'), 'utf-8');
+    let eventsJSON = JSON.parse(events);
+    let eventMaxId = eventsJSON.findLast((e) => e.id);
+    let event = {
+        id: eventMaxId.id + 1,
+        title: eventToAdd.eventTitle,
+        description: eventToAdd.eventDescription,
+        category: eventToAdd.eventCategory,
+        location: eventToAdd.eventLocation,
+        startTime: eventToAdd.eventStartTime,
+        endTime: eventToAdd.eventEndTime
+    };
+    eventsJSON.push(event);
+    let result = fs.writeFileSync(path.join(__dirPath, '../data/events.json'), JSON.stringify(eventsJSON), 'utf-8')
+    return (result);
+}
